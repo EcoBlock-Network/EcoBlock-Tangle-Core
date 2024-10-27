0ecf12e39355fa86a72b8aae66b7462999413f1c
Winrhy <malo@arkada.fr>
2024-10-27 11:00:40 +0100

    refactor: :recycle: refactor Method to add a peer to the network and impl

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,21 +1,31 @@
+use super::communication::Communication;
+
 pub mod peer;
 pub mod message;
 pub mod communication;
 
-
 pub struct Network {
-    pub peers: Vec<peer::Peer>
+    pub peers: Vec<peer::Peer>,
+    communication: Communication
 }
 
 impl Network {
     pub fn new() -> Self {
         Network {
-            peers: Vec::new()
+            peers: Vec::new(),
+            communication: Communication::new()
         }
     }
 
     //Method to add a peer to the network
     pub fn add_peer(&mut self, peer: peer::Peer) {
-        self.peers.push(peer);
+        self.peers.push(peer.clone());
+        self.communication.connect_peer(peer);
+    }
+
+    //Method to remove a peer from the network
+    pub fn remove_peer(&mut self, peer_id : &str){
+        self.peers.retain(|p| p.id != peer_id);
+        self.communication.disconnect_peer(peer_id);
     }
 }
