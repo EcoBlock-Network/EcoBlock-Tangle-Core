6e96b4580b3d0dc2a25ccab61257fde80f3e0fe3
Winrhy <malo@arkada.fr>
2024-10-27 10:30:02 +0100

    feat: :sparkles: Impl peer

--- src/network/peer.rs
+++ src/network/peer.rs
@@ -3,3 +3,12 @@ pub struct Peer {
     pub id: String,
     pub address: String
 }
+
+impl Peer {
+    pub fn new(id: &str, address: &str) -> Self {
+        Peer {
+            id : id.to_string(),
+            address : address.to_string()
+        }
+    }
+}
