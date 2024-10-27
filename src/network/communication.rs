552fe0b74fd251a455f9a1a0cafab737540db4c2
Winrhy <malo@arkada.fr>
2024-10-27 10:45:36 +0100

    feat: :sparkles: Impl Communication and //Create a new empty communication

--- src/network/communication.rs
+++ src/network/communication.rs
@@ -3,6 +3,15 @@ use super::message::Message;
 
 #[derive(Debug)]
 
-pub struct communication{
+pub struct Communication{
     connected_peers: Vec<Peer>
+}
+
+impl Communication{
+    //Create a new empty communication
+    pub fn new() -> Self{
+        Communication{
+            connected_peers: Vec::new()
+        }
+    }
 }
