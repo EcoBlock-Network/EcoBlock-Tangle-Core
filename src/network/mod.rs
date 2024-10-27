2f7607b0c42bcf83cd5e1586bec5615171588c38
Winrhy <malo@arkada.fr>
2024-10-27 10:31:27 +0100

    feat: :sparkles: Add method to add a peer to a network

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -5,3 +5,16 @@ pub mod message;
 pub struct Network {
     peers: Vec<peer::Peer>
 }
+
+impl Network {
+    pub fn new() -> Self {
+        Network {
+            peers: Vec::new()
+        }
+    }
+
+    //Method to add a peer to the network
+    pub fn add_peer(&mut self, peer: peer::Peer) {
+        self.peers.push(peer);
+    }
+}
