725c1442c4118325bbcd99971f8d04bbf952fa60
Winrhy <malo@arkada.fr>
2024-10-27 10:46:48 +0100

    feat: :sparkles: Add Method to add a peer to the list of connected peers

--- src/network/communication.rs
+++ src/network/communication.rs
@@ -14,4 +14,10 @@ impl Communication{
             connected_peers: Vec::new()
         }
     }
+
+    //Method to add a peer to the list of connected peers
+    pub fn connect_peer(&mut self, peer: Peer){
+        print!("Peer connected: {:?}", peer);
+        self.connected_peers.push(peer);
+    }
 }
